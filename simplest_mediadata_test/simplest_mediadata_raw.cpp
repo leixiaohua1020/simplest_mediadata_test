a5c6a4e8aaf1b416aeb7a90cd01ea4b0d588d23f
Lei Xiaohua <leixiaohua1020@126.com>
2016-01-11 23:53:59 +0800

    add pcm2wav

--- simplest_mediadata_test/simplest_mediadata_raw.cpp
+++ simplest_mediadata_test/simplest_mediadata_raw.cpp
@@ -777,11 +777,108 @@ int simplest_pcm16le_to_pcm8(char *url){
 	return 0;
 }
 
+/**
+ * Convert PCM16LE raw data to WAVE format
+ * @param pcmpath      Input PCM file.
+ * @param channels     Channel number of PCM file.
+ * @param sample_rate  Sample rate of PCM file.
+ * @param wavepath     Output WAVE file.
+ */
+int simplest_pcm16le_to_wave(const char *pcmpath,int channels,int sample_rate,const char *wavepath)
+{
+	typedef struct WAVE_HEADER{
+		char fccID[4];
+		unsigned long dwSize;
+		char fccType[4];
+	}WAVE_HEADER;
+
+	typedef struct WAVE_FMT{
+		char  fccID[4];
+		unsigned long dwSize;
+		unsigned short wFormatTag;
+		unsigned short wChannels;
+		unsigned long dwSamplesPerSec;
+		unsigned long dwAvgBytesPerSec;
+		unsigned short wBlockAlign;
+		unsigned short uiBitsPerSample;
+	}WAVE_FMT;
+
+	typedef struct WAVE_DATA{
+		char       fccID[4];
+		unsigned long dwSize;
+	}WAVE_DATA;
+
+
+	if(channels==0||sample_rate==0){
+		channels = 2;
+		sample_rate = 44100;
+	}
+	int bits = 16;
+
+    WAVE_HEADER pcmHEADER;
+    WAVE_FMT pcmFMT;
+    WAVE_DATA pcmDATA;
+
+    unsigned short m_pcmData;
+    FILE *fp,*fpout;
+
+	fp=fopen(pcmpath,"rb+");
+    if(fp == NULL) {
+        printf("Open pcm file error\n");
+        return -1;
+    }
+	fpout=fopen(wavepath,"wb+");
+    if(fpout == NULL) {
+        printf("Create wav file error\n");
+        return -1;
+    }
+	//WAVE_HEADER
+    memcpy(pcmHEADER.fccID,"RIFF",strlen("RIFF"));
+    memcpy(pcmHEADER.fccType,"WAVE",strlen("WAVE"));
+    fseek(fpout,sizeof(WAVE_HEADER),1);
+	//WAVE_FMT
+    pcmFMT.dwSamplesPerSec=sample_rate;
+    pcmFMT.dwAvgBytesPerSec=pcmFMT.dwSamplesPerSec*sizeof(m_pcmData);
+    pcmFMT.uiBitsPerSample=bits;
+    memcpy(pcmFMT.fccID,"fmt ",strlen("fmt "));
+    pcmFMT.dwSize=16;
+    pcmFMT.wBlockAlign=2;
+    pcmFMT.wChannels=channels;
+    pcmFMT.wFormatTag=1;
+
+    fwrite(&pcmFMT,sizeof(WAVE_FMT),1,fpout);
+
+    //WAVE_DATA;
+    memcpy(pcmDATA.fccID,"data",strlen("data"));
+    pcmDATA.dwSize=0;
+    fseek(fpout,sizeof(WAVE_DATA),SEEK_CUR);
+
+    fread(&m_pcmData,sizeof(unsigned short),1,fp);
+    while(!feof(fp)){
+        pcmDATA.dwSize+=2;
+        fwrite(&m_pcmData,sizeof(unsigned short),1,fpout);
+        fread(&m_pcmData,sizeof(unsigned short),1,fp);
+    }
+
+    pcmHEADER.dwSize=44+pcmDATA.dwSize;
+
+    rewind(fpout);
+    fwrite(&pcmHEADER,sizeof(WAVE_HEADER),1,fpout);
+    fseek(fpout,sizeof(WAVE_FMT),SEEK_CUR);
+    fwrite(&pcmDATA,sizeof(WAVE_DATA),1,fpout);
+	
+	fclose(fp);
+    fclose(fpout);
+
+    return 0;
+}
+
+
 
 int main(int argc, char* argv[]){
 
 	//Test
-	
+
 	simplest_yuv420_split("lena_256x256_yuv420p.yuv",256,256,1);
 
 	simplest_yuv444_split("lena_256x256_yuv444p.yuv",256,256,1);
@@ -813,6 +910,7 @@ int main(int argc, char* argv[]){
 	simplest_pcm16le_to_pcm8("NocturneNo2inEflat_44.1k_s16le.pcm");
 
 	simplest_pcm16le_cut_singlechannel("drum.pcm",2360,120);
+
 	
 	simplest_h264_parser("sintel.h264");
 	
@@ -821,6 +919,9 @@ int main(int argc, char* argv[]){
 	simplest_aac_parser("nocturne.aac");
 	
 	simplest_udp_parser(8880);
+
+	simplest_pcm16le_to_wave("NocturneNo2inEflat_44.1k_s16le.pcm",2,44100,"output_nocturne.wav");
+
 
 	return 0;
 }
